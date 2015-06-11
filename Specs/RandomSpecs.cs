3eb0a2cb0850131d9422278621145ba4d2a71a06
Angel Lakov <alakov@neworbit.co.uk>
2015-06-11 21:20:26 +0100

    Added Unit-Testing framework

---
+++ Specs/RandomSpecs.cs
@@ -0,0 +1,22 @@
+﻿// ReSharper disable UnusedMember.Local
+namespace Specs
+{
+    using System;
+
+    using Machine.Fakes;
+    using Machine.Specifications;
+
+    public class Random_test : WithSubject<Random>
+    {
+        private static int result;
+
+        private Establish context = () => { };
+
+        private Because of = () => { result = Subject.Next(0, 10); };
+        
+        private It shouldBeZeroOrMore = () => result.ShouldBeGreaterThanOrEqualTo(0);
+
+        private It shouldBeNineOrLess = () => result.ShouldBeLessThan(10);
+    }
+}
+// ReSharper restore UnusedMember.Local
